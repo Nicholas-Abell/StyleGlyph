496078b0c9602fb8b2e2529717a45d8478183e26
Nick <darkforceyoda@gmail.com>
2024-03-15 17:13:02 -0500

    installed shadcn

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -1,9 +1,22 @@
+import Link from "next/link";
 import "./styles.scss";
 
 export default function Home() {
   return (
     <main>
-      <div className="💪👆🏻  😶‍🌫️ gap-4">
+      <nav className="💪 justify-center items-center gap-4">
+        <Link className="hover:text-gray-400" href="/docs">
+          Docs
+        </Link>
+        <Link className="hover:text-gray-400" href="/getstarted">
+          Blog
+        </Link>
+        <Link className="hover:text-gray-400" href="/about">
+          Showcase
+        </Link>
+        
+      </nav>
+      <div className="💪  😶‍🌫️ gap-4">
         <p>hello</p>
         <p>world</p>
       </div>
