f7d081d94c924476e966d82610012c08f85b2829
Nick <darkforceyoda@gmail.com>
2024-03-15 10:36:11 -0500

    init

---
+++ sitex/app/page.tsx
@@ -0,0 +1,12 @@
+import "./styles.scss";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="💪👆🏻  😶‍🌫️ gap-4">
+        <p>hello</p>
+        <p>world</p>
+      </div>
+    </main>
+  );
+}
