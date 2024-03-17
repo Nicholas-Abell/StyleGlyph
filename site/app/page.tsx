c35a967c1a838a6bad1c1e7a8245fdb27a5ad304
Nick <darkforceyoda@gmail.com>
2024-03-16 20:41:45 -0500

    hero section layout

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -1,25 +1,36 @@
-import Link from "next/link";
 import "./styles.scss";
+import { Button } from "@/components/ui/button";
+import { FaSearch } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main>
-      <nav className="💪 justify-center items-center gap-4">
-        <Link className="hover:text-gray-400" href="/docs">
-          Docs
-        </Link>
-        <Link className="hover:text-gray-400" href="/getstarted">
-          Blog
-        </Link>
-        <Link className="hover:text-gray-400" href="/about">
-          Showcase
-        </Link>
-        
-      </nav>
-      <div className="💪  😶‍🌫️ gap-4">
-        <p>hello</p>
-        <p>world</p>
-      </div>
-    </main>
+    <>
+      <main>
+        <div className="ml-auto mr-auto max-w-[480px] py-24">
+          <h2 className="text-4xl md:text-6xl font-bold">
+            Be 😁 building websites 🏎️ <em className="text-xs">quickly</em> with
+            emojis and style
+          </h2>
+          <p className="py-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo dolorum
+            similique iste nobis aperiam nostrum
+          </p>
+          <div className="grid grid-cols-3 w-full items-center gap-4">
+            <Button variant="default" className="font-bold">
+              Get Started
+            </Button>
+            <Button
+              className="col-span-2 flex gap-2 items-center text-slate-400 border-slate-500"
+              variant="outline"
+            >
+              <FaSearch />
+              Quick search...
+            </Button>
+          </div>
+        </div>
+      </main>
+      <section>
+      </section>
+    </>
   );
 }
