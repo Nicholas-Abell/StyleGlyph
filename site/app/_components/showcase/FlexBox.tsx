ee2b8bfbd0fc1571e8023b4f7b3f8674a046bb9f
Nick <darkforceyoda@gmail.com>
2024-03-20 17:14:35 -0500

    Showcase mobile view

--- site/app/_components/showcase/FlexBox.tsx
+++ site/app/_components/showcase/FlexBox.tsx
@@ -1,8 +1,33 @@
+"use client";
 import React from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 type FlexBoxProps = {};
 
 const FlexBox: React.FC<FlexBoxProps> = () => {
+  const [api, setApi] = React.useState<CarouselApi>();
+  const [current, setCurrent] = React.useState(0);
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
   return (
     <section className="p-4">
       <div className="flex items-center justify-center w-full">
@@ -10,104 +35,221 @@ const FlexBox: React.FC<FlexBoxProps> = () => {
           <h3 className="text-4xl text-slate-50 font-bold">💪 BOX</h3>
         </div>
       </div>
-      <div className="grid grid-cols-3 gap-2 pt-4">
-        <div className="flex justify-center items-center w-full bg-slate-800 rounded-lg">
-          <code className="text-sm text-slate-50 p-4">
-            <p className="underline">Flex Row</p>
-            &lt;<span className="html-tag">div</span>{" "}
-            <span className="class-name">class</span>=&quot;
-            <span className="class-name">💪</span>
-            &quot;&gt;
-            <br />
-            <span className="hide">...</span>&lt;
-            <span className="html-tag">div</span>{" "}
-            <span className="class-name">class</span>=&quot;
-            <span className="class-name">🟥</span>&quot;/&gt;
-            <br />
-            <span className="hide">...</span>
-            &lt;<span className="html-tag">div</span>{" "}
-            <span className="class-name">class</span>=&quot;
-            <span className="class-name">🟦</span>&quot;/&gt;
-            <br />
-            <span className="hide">...</span>
-            &lt;<span className="html-tag">div</span>{" "}
-            <span className="class-name">class</span>=&quot;
-            <span className="class-name">🟩</span>&quot;/&gt;
-            <br />
-            &lt;/
-            <span className="html-tag">div</span>&gt;
-          </code>
-        </div>
-        <div className="flex justify-center items-center w-full bg-slate-800 rounded-lg">
-          <code className="text-sm text-slate-50 p-4">
-            <p className="underline">Flex Column</p>
-            &lt;<span className="html-tag">div</span>{" "}
-            <span className="class-name">class</span>=&quot;
-            <span className="class-name">💪👇</span>
-            &quot;&gt;
-            <br />
-            <span className="hide">...</span>&lt;
-            <span className="html-tag">div</span>{" "}
-            <span className="class-name">class</span>=&quot;
-            <span className="class-name">🟥</span>&quot;/&gt;
-            <br />
-            <span className="hide">...</span>
-            &lt;<span className="html-tag">div</span>{" "}
-            <span className="class-name">class</span>=&quot;
-            <span className="class-name">🟦</span>&quot;/&gt;
-            <br />
-            <span className="hide">...</span>
-            &lt;<span className="html-tag">div</span>{" "}
-            <span className="class-name">class</span>=&quot;
-            <span className="class-name">🟩</span>&quot;/&gt;
-            <br />
-            &lt;/
-            <span className="html-tag">div</span>&gt;
-          </code>
-        </div>
-        <div className="flex justify-center items-center w-full bg-slate-800 rounded-lg">
-          <code className="text-sm text-slate-50 p-4">
-            <p className="underline">Flex Row-Reverse</p>
-            &lt;<span className="html-tag">div</span>{" "}
-            <span className="class-name">class</span>=&quot;
-            <span className="class-name">💪👈</span>
-            &quot;&gt;
-            <br />
-            <span className="hide">...</span>&lt;
-            <span className="html-tag">div</span>{" "}
-            <span className="class-name">class</span>=&quot;
-            <span className="class-name">🟥</span>&quot;/&gt;
-            <br />
-            <span className="hide">...</span>
-            &lt;<span className="html-tag">div</span>{" "}
-            <span className="class-name">class</span>=&quot;
-            <span className="class-name">🟦</span>&quot;/&gt;
-            <br />
-            <span className="hide">...</span>
-            &lt;<span className="html-tag">div</span>{" "}
-            <span className="class-name">class</span>=&quot;
-            <span className="class-name">🟩</span>&quot;/&gt;
-            <br />
-            &lt;/
-            <span className="html-tag">div</span>&gt;
-          </code>
+      {/* Mobile */}
+      <div className="w-full grid md:hidden border items-center justify-center px-8">
+        <Carousel setApi={setApi} className="py-2">
+          <CarouselContent>
+            <CarouselItem className="grid grid-cols-1 gap-4">
+              <div className="flex justify-center items-center w-full bg-slate-800 rounded-lg">
+                <code className="text-sm text-slate-50 p-4">
+                  <p className="underline">Flex Row</p>
+                  &lt;<span className="html-tag">div</span>{" "}
+                  <span className="class-name">class</span>=&quot;
+                  <span className="class-name">💪</span>
+                  &quot;&gt;
+                  <br />
+                  <span className="hide">...</span>&lt;
+                  <span className="html-tag">div</span>{" "}
+                  <span className="class-name">class</span>=&quot;
+                  <span className="class-name">🟥</span>&quot;/&gt;
+                  <br />
+                  <span className="hide">...</span>
+                  &lt;<span className="html-tag">div</span>{" "}
+                  <span className="class-name">class</span>=&quot;
+                  <span className="class-name">🟦</span>&quot;/&gt;
+                  <br />
+                  <span className="hide">...</span>
+                  &lt;<span className="html-tag">div</span>{" "}
+                  <span className="class-name">class</span>=&quot;
+                  <span className="class-name">🟩</span>&quot;/&gt;
+                  <br />
+                  &lt;/
+                  <span className="html-tag">div</span>&gt;
+                </code>
+              </div>
+              <div className="border border-gray-300 rounded-lg p-4 flex gap-4 items-center">
+                <div className="🟥" />
+                <div className="🟦" />
+                <div className="🟩" />
+              </div>
+            </CarouselItem>
+            <CarouselItem className="grid grid-cols-1 gap-4">
+              <div className="flex justify-center items-center w-full bg-slate-800 rounded-lg">
+                <code className="text-sm text-slate-50 p-4">
+                  <p className="underline">Flex Column</p>
+                  &lt;<span className="html-tag">div</span>{" "}
+                  <span className="class-name">class</span>=&quot;
+                  <span className="class-name">💪👇</span>
+                  &quot;&gt;
+                  <br />
+                  <span className="hide">...</span>&lt;
+                  <span className="html-tag">div</span>{" "}
+                  <span className="class-name">class</span>=&quot;
+                  <span className="class-name">🟥</span>&quot;/&gt;
+                  <br />
+                  <span className="hide">...</span>
+                  &lt;<span className="html-tag">div</span>{" "}
+                  <span className="class-name">class</span>=&quot;
+                  <span className="class-name">🟦</span>&quot;/&gt;
+                  <br />
+                  <span className="hide">...</span>
+                  &lt;<span className="html-tag">div</span>{" "}
+                  <span className="class-name">class</span>=&quot;
+                  <span className="class-name">🟩</span>&quot;/&gt;
+                  <br />
+                  &lt;/
+                  <span className="html-tag">div</span>&gt;
+                </code>
+              </div>
+              <div className="💪👇 border border-gray-300 rounded-lg p-4 gap-4 items-center">
+                <div className="🟥" />
+                <div className="🟦" />
+                <div className="🟩" />
+              </div>
+            </CarouselItem>
+            <CarouselItem className="grid grid-cols-1 gap-4">
+              <div className="flex justify-center items-center w-full bg-slate-800 rounded-lg">
+                <code className="text-sm text-slate-50 p-4">
+                  <p className="underline">Flex Row-Reverse</p>
+                  &lt;<span className="html-tag">div</span>{" "}
+                  <span className="class-name">class</span>=&quot;
+                  <span className="class-name">💪👈</span>
+                  &quot;&gt;
+                  <br />
+                  <span className="hide">...</span>&lt;
+                  <span className="html-tag">div</span>{" "}
+                  <span className="class-name">class</span>=&quot;
+                  <span className="class-name">🟥</span>&quot;/&gt;
+                  <br />
+                  <span className="hide">...</span>
+                  &lt;<span className="html-tag">div</span>{" "}
+                  <span className="class-name">class</span>=&quot;
+                  <span className="class-name">🟦</span>&quot;/&gt;
+                  <br />
+                  <span className="hide">...</span>
+                  &lt;<span className="html-tag">div</span>{" "}
+                  <span className="class-name">class</span>=&quot;
+                  <span className="class-name">🟩</span>&quot;/&gt;
+                  <br />
+                  &lt;/
+                  <span className="html-tag">div</span>&gt;
+                </code>
+              </div>
+              <div className="💪👈 border border-gray-300 rounded-lg p-4 gap-4 items-center">
+                <div className="🟥" />
+                <div className="🟦" />
+                <div className="🟩" />
+              </div>
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+        <div className="py-2 text-center text-sm text-muted-foreground">
+          Slide {current} of {count}
         </div>
       </div>
-      <div className="grid grid-cols-3 gap-2 pt-2">
-        <div className="border border-gray-300 rounded-lg p-4 flex gap-4 items-center">
-          <div className="🟥" />
-          <div className="🟦" />
-          <div className="🟩" />
+      {/* Desktop */}
+      <div className="hidden md:block">
+        <div className="grid grid-cols-3 gap-2 pt-4 ">
+          <div className="flex justify-center items-center w-full bg-slate-800 rounded-lg">
+            <code className="text-sm text-slate-50 p-4">
+              <p className="underline">Flex Row</p>
+              &lt;<span className="html-tag">div</span>{" "}
+              <span className="class-name">class</span>=&quot;
+              <span className="class-name">💪</span>
+              &quot;&gt;
+              <br />
+              <span className="hide">...</span>&lt;
+              <span className="html-tag">div</span>{" "}
+              <span className="class-name">class</span>=&quot;
+              <span className="class-name">🟥</span>&quot;/&gt;
+              <br />
+              <span className="hide">...</span>
+              &lt;<span className="html-tag">div</span>{" "}
+              <span className="class-name">class</span>=&quot;
+              <span className="class-name">🟦</span>&quot;/&gt;
+              <br />
+              <span className="hide">...</span>
+              &lt;<span className="html-tag">div</span>{" "}
+              <span className="class-name">class</span>=&quot;
+              <span className="class-name">🟩</span>&quot;/&gt;
+              <br />
+              &lt;/
+              <span className="html-tag">div</span>&gt;
+            </code>
+          </div>
+          <div className="flex justify-center items-center w-full bg-slate-800 rounded-lg">
+            <code className="text-sm text-slate-50 p-4">
+              <p className="underline">Flex Column</p>
+              &lt;<span className="html-tag">div</span>{" "}
+              <span className="class-name">class</span>=&quot;
+              <span className="class-name">💪👇</span>
+              &quot;&gt;
+              <br />
+              <span className="hide">...</span>&lt;
+              <span className="html-tag">div</span>{" "}
+              <span className="class-name">class</span>=&quot;
+              <span className="class-name">🟥</span>&quot;/&gt;
+              <br />
+              <span className="hide">...</span>
+              &lt;<span className="html-tag">div</span>{" "}
+              <span className="class-name">class</span>=&quot;
+              <span className="class-name">🟦</span>&quot;/&gt;
+              <br />
+              <span className="hide">...</span>
+              &lt;<span className="html-tag">div</span>{" "}
+              <span className="class-name">class</span>=&quot;
+              <span className="class-name">🟩</span>&quot;/&gt;
+              <br />
+              &lt;/
+              <span className="html-tag">div</span>&gt;
+            </code>
+          </div>
+          <div className="flex justify-center items-center w-full bg-slate-800 rounded-lg">
+            <code className="text-sm text-slate-50 p-4">
+              <p className="underline">Flex Row-Reverse</p>
+              &lt;<span className="html-tag">div</span>{" "}
+              <span className="class-name">class</span>=&quot;
+              <span className="class-name">💪👈</span>
+              &quot;&gt;
+              <br />
+              <span className="hide">...</span>&lt;
+              <span className="html-tag">div</span>{" "}
+              <span className="class-name">class</span>=&quot;
+              <span className="class-name">🟥</span>&quot;/&gt;
+              <br />
+              <span className="hide">...</span>
+              &lt;<span className="html-tag">div</span>{" "}
+              <span className="class-name">class</span>=&quot;
+              <span className="class-name">🟦</span>&quot;/&gt;
+              <br />
+              <span className="hide">...</span>
+              &lt;<span className="html-tag">div</span>{" "}
+              <span className="class-name">class</span>=&quot;
+              <span className="class-name">🟩</span>&quot;/&gt;
+              <br />
+              &lt;/
+              <span className="html-tag">div</span>&gt;
+            </code>
+          </div>
         </div>
-        <div className="💪👇 border border-gray-300 rounded-lg p-4 gap-4 items-center">
-          <div className="🟥" />
-          <div className="🟦" />
-          <div className="🟩" />
-        </div>
-        <div className="💪👈 border border-gray-300 rounded-lg p-4 gap-4 items-center">
-          <div className="🟥" />
-          <div className="🟦" />
-          <div className="🟩" />
+        <div className="grid grid-cols-3 gap-2 pt-2">
+          <div className="border border-gray-300 rounded-lg p-4 flex gap-4 items-center">
+            <div className="🟥" />
+            <div className="🟦" />
+            <div className="🟩" />
+          </div>
+          <div className="💪👇 border border-gray-300 rounded-lg p-4 gap-4 items-center">
+            <div className="🟥" />
+            <div className="🟦" />
+            <div className="🟩" />
+          </div>
+          <div className="💪👈 border border-gray-300 rounded-lg p-4 gap-4 items-center">
+            <div className="🟥" />
+            <div className="🟦" />
+            <div className="🟩" />
+          </div>
         </div>
       </div>
     </section>
